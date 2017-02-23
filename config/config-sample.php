d7395696fe215c9081ddc874dae22feb482ec00a
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2017-02-23 11:16:02 +0100

    dependencies and config added

---
+++ config/config-sample.php
@@ -0,0 +1,12 @@
+define('DB_NAME', 'database name');
+define('DB_USER', 'database user');
+define('DB_PASSWORD', 'database password');
+define('DB_HOST', 'database host');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+define('LOGGED_IN_KEY',    'enter salt');
+define('AUTH_SALT',        'enter salt');
+define('LOGGED_IN_SALT',   'enter salt');
+
+$table_prefix  = 'site17_';
