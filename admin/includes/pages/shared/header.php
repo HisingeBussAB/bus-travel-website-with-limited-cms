aaac4be8188c0bda1b5c1af5cd6317f24feb8efe
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2017-03-31 14:14:15 +0200

    admin pages

---
+++ admin/includes/pages/shared/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="author" content="Håkan K Arnoldson">
+  <title>Rekå Resor - <?php echo $pageTitle ?></title>
+
+  <meta name="robots" content="noindex, nofollow">
+
+  <link rel="icon" href="/favicon/favicon.ico">
+
+        <link rel="stylesheet" href="http<?php echo APPEND_SSL ?>://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="http<?php echo APPEND_SSL ?>://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="/admin/css/main.min.css" rel="stylesheet">
+
+  <script src="http<?php echo APPEND_SSL ?>://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="/dependencies/jquery-3.1.1/jquery-3.1.1.min.js"><\/script>')</script>
+  <script src="http<?php echo APPEND_SSL ?>://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script>
+    if(typeof($.fn.modal) === 'undefined') {
+      document.write('<script src="/dependencies/bootstrap-3.3.7-dist/js/bootstrap.min.js"><\/script>');
+      $("head").prepend('<link rel="stylesheet" href="/dependencies/bootstrap-3.3.7-dist/css/bootstrap.min.css" />');
+      $("head").prepend('<link rel="stylesheet" href="/dependencies/font-awesome-4.7.0/css/font-awesome.min.css" />');
+    }
+  </script>
+
+</head>
+<body>
+  <header>
+    <h1>Rekå Resor - Administration</h1>
+  </header>
