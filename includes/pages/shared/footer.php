5061e3d3455ffa8d3a60de6406a0a0b53458f5e1
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2017-08-29 12:45:57 +0200

    server reboot

--- includes/pages/shared/footer.php
+++ includes/pages/shared/footer.php
@@ -16,40 +16,52 @@ if (!empty($morestyles)) echo $morestyles;
 
 
 
-<footer class="clearfix container main-footer">
-  <section>
-    <h2 class='footer-heading'>Vill du ha information om våra resor?</h2>
-    <form>
-      <input type="email" placeholder="E-post...">
-      <input type="submit" value="Prenumerera på vårt nyhetsbrev">
-    </form>
-    <div><a href="#">Följ oss på Facebook</a></div>
+<footer class="clearfix container main-footer text-center">
+
+
+
+
+
+  <section class="text-center container-fluid clearfix">
+    <div class="row-fluid">
+      <section class='col-md-6 col-xs-12'>
+        <h2 class='footer-heading'>Vill du ha information om våra resor?</h2>
+        <form>
+          <input type="email" placeholder="E-post...">
+          <input type="submit" value="Prenumerera">
+        </form>
+        <figure>
+          <a href="https://www.facebook.com/rekoresor/" target="_blank"><img src='/img/facebook.png' alt="Besök oss på Facebook" /><span class='sr-only'>Besök vår Facebooksida</span></a>
+        </figure>
+      </section>
+      <section class='col-md-6 col-xs-12'>
+      <h2 class='footer-heading'>Företag i koncernen</h2>
+      <div class='koncern-logo-container'>
+        <figure class="koncern-logo text-center">
+          <img src="/img/hb-logga.png" alt="Hisinge Buss AB" />
+          <span class='sr-only'>Hisinge Buss AB</span>
+        </figure>
+        <figure class="koncern-logo text-center">
+          <img src="/img/reka-logga.jpg" alt="Rekå Resor AB" />
+          <span class='sr-only'>Rekå Resor AB</span>
+        </figure>
+        <figure class="koncern-logo text-center">
+          <img src="/img/bp-logga.png" alt="Buss- och Resepoolen i Göteborg AB" />
+          <span class='sr-only'>Buss- och Resepoolen i Göteborg AB</span>
+        </figure>
+      </div>
+      </section>
+    </div>
+
+
+
+
   </section>
-  <address>
-    Telefon: <a rel="nofollow" href="tel:+4631222120">031-22 21 20</a> | E-post: <a rel="nofollow" href="mailto:info@rekoresor.se">info@rekoresor.se</a><br>
-    Aröds Industriväg 30, 422 43 Hisings Backa | Box 8797, 402 76 Göteborg
+
+  <address class='text-center'>
+      <p>Telefon: <a rel="nofollow" href="tel:+4631222120" class="white-link">031-22 21 20</a> | E-post: <a rel="nofollow" href="mailto:info@rekoresor.se" class="white-link">info@rekoresor.se</a></p>
+      <p>Besöksadress: Aröds Industriväg 30, 422 43 Hisings Backa | Postadress: Box 8797, 402 76 Göteborg</p>
   </address>
-
-
-  <section class="text-center container-fluid">
-    <div class="row-fluid">
-      <h2 class='footer-heading col-md-12'>Företag i koncernen</h2>
-    </div>
-    <div class="row-fluid">
-      <figure class="koncern-logo text-center">
-        <img src="/img/hb-logga.png" alt="Hisinge Buss AB" />
-        <span class='sr-only'>Hisinge Buss AB</span>
-      </figure>
-      <figure class="koncern-logo text-center">
-        <img src="/img/reka-logga.jpg" alt="Rekå Resor AB" />
-        <span class='sr-only'>Rekå Resor AB</span>
-      </figure>
-      <figure class="koncern-logo text-center">
-        <img src="/img/bp-logga.png" alt="Buss- och Resepoolen i Göteborg AB" />
-        <span class='sr-only'>Buss- och Resepoolen i Göteborg AB</span>
-      </figure>
-    </div>
-  </section>
 
 </footer>
 
